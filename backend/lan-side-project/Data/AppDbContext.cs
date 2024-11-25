5f441676f1eeb6f322835b6f50c8ddd1507a275f
Lan <s35241607s@gmail.com>
2024-11-25 21:04:03 +0800

    修改專案架構

---
+++ backend/lan-side-project/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using lan_side_project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lan_side_project.Data
+{
+    public partial class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+    }
+}
---
+++ backend/lan-side-project/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace lan_side_project.Models
+{
+    public class Role : AuditBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public ICollection<Permission> Permissions { get; set; } = [];
+    }
+}
