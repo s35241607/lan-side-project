dce8fada409af455f51ff76cc84341c70fa081a6
Lan <s35241607s@gmail.com>
2024-11-26 19:37:01 +0800

    新增 model creating 讓 migration 的時候是使用 snake_case

--- backend/lan-side-project/Data/AppDbContext.cs
+++ backend/lan-side-project/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using lan_side_project.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace lan_side_project.Data
 {
@@ -8,5 +9,32 @@ namespace lan_side_project.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Permission> Permissions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // 使用 snake_case 命名規則
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                // 轉換表格名稱為 snake_case
+                entity.SetTableName(ToSnakeCase(entity.GetTableName()!));
+
+                // 轉換每個欄位名稱為 snake_case
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(ToSnakeCase(property.Name));
+                }
+            }
+        }
+
+        // 轉換名稱為 snake_case
+        private static string ToSnakeCase(string str)
+        {
+            return Regex.Replace(
+                str,
+                @"([a-z])([A-Z])",
+                "$1_$2").ToLower();
+        }
     }
 }
