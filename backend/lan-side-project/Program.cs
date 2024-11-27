37e0337fb8800e912d4c1565159c1cb1fc2bc815
Lan <s35241607s@gmail.com>
2024-11-27 21:09:43 +0800

    add postgres to docker-compose.yml

--- backend/lan-side-project/Program.cs
+++ backend/lan-side-project/Program.cs
@@ -11,59 +11,72 @@ namespace lan_side_project
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // 從 appsettings.json 讀取 Serilog 設定
-            builder.Host.UseSerilog((context, services, configuration) =>
-                configuration.ReadFrom.Configuration(context.Configuration));
-
-            // 配置 EF Core 與 PostgreSQL
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            app.UseSerilogHttpSessionsLogging(HttpSessionInfoToLog.All);
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            Serilog.Debugging.SelfLog.Enable(Console.Out);
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                Log.Information("Starting web host");
+                var builder = WebApplication.CreateBuilder(args);
 
-            app.UseHttpsRedirection();
+                // 從 appsettings.json 讀取 Serilog 設定
+                builder.Host.UseSerilog((context, services, configuration) =>
+                    configuration.ReadFrom.Configuration(context.Configuration));
 
-            app.UseAuthorization();
+                // 配置 EF Core 與 PostgreSQL
+                builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            var summaries = new[]
-            {
+                // Add services to the container.
+                builder.Services.AddAuthorization();
+
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                app.UseSerilogHttpSessionsLogging(HttpSessionInfoToLog.All);
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                var summaries = new[]
+                {
                 "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
             };
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
+                app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+                {
+                    var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                        .ToArray();
+                    return forecast;
+                })
+                .WithName("GetWeatherForecast")
+                .WithOpenApi();
 
-            app.Run();
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
