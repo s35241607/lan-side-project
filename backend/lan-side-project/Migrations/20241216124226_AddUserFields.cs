1b8f62e12602526e411bd2d85707d227d5bd3674
Lan <s35241607s@gmail.com>
2024-12-16 20:48:40 +0800

    feat: add db migrations about user login and reset password

---
+++ backend/lan-side-project/Migrations/20241216124226_AddUserFields.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lan_side_project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_login_date",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_password_changed_date",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_password_reset_date",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "login_failed_attempts",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "login_lockout_end",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "reset_password_failed_attempts",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "reset_password_lockout_end",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "reset_password_token",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "reset_password_token_expiration",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_login_date",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "last_password_changed_date",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "last_password_reset_date",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "login_failed_attempts",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "login_lockout_end",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "reset_password_failed_attempts",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "reset_password_lockout_end",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "reset_password_token",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "reset_password_token_expiration",
+                table: "users");
+        }
+    }
+}
