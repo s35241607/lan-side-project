0637e36916e84e2dff684ecc3ee5783cc4b6d5d4
ChiuZhenHuang <cc45001127@gmail.com>
2024-12-17 15:43:50 +0800

    增加navbar

--- frontend/react/src/components/layout.tsx
+++ frontend/react/src/components/layout.tsx
@@ -7,7 +7,7 @@ import {
   VideoCameraOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
-import { Button, Layout, Menu, theme, message } from "antd";
+import { Button, Layout, Menu, message } from "antd";
 import { useNavigate } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 
@@ -18,9 +18,6 @@ interface LayoutProps {
 const LayoutComponent: React.FC<LayoutProps> = ({ children }) => {
   const navigate = useNavigate();
   const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
 
   const logOutHandler = (): void => {
     document.cookie = "token=; max-age=0; path=/;";
@@ -30,7 +27,14 @@ const LayoutComponent: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <Layout className="h-screen">
-      <Sider trigger={null} collapsible collapsed={collapsed}>
+      <Sider
+        trigger={null}
+        collapsed={collapsed}
+        breakpoint="lg"
+        // breakpoint="xs" // 設定斷點為最小寬度
+        // className="hidden xs:block"
+        collapsedWidth="0"
+      >
         <div className="demo-logo-vertical" />
         <Menu
           theme="dark"
@@ -65,30 +69,48 @@ const LayoutComponent: React.FC<LayoutProps> = ({ children }) => {
           ]}
         />
       </Sider>
+
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Header className="flex items-center justify-between px-4 bg-slate-200 font-bold border-b border-gray-300">
           <Button
             type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
-            }}
           />
+
+          <Menu
+            mode="horizontal"
+            theme="light"
+            defaultSelectedKeys={["1"]}
+            className="flex-1 ml-4 justify-center bg-transparent sticky hidden border-0 sm:flex"
+            items={[
+              {
+                key: "1",
+                label: "基本資料",
+              },
+              {
+                key: "2",
+                label: "我的作品",
+              },
+              {
+                key: "3",
+                label: "新增作品",
+              },
+            ]}
+          />
+
+          <Button
+            type="primary"
+            onClick={logOutHandler}
+            className="hidden sm:block"
+          >
+            登出
+          </Button>
         </Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <div className="w-full h-full overflow-hidden">
-            <div className="max-w-full whitespace-normal">{children}</div>
+
+        <Content className="bg-slate-200">
+          <div className="w-full h-full overflow-y-auto">
+            <div className="max-w-full whitespace-normal p-4">{children}</div>
           </div>
         </Content>
       </Layout>
